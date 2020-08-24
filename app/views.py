0ffb7fce9aea485cdc69cc6f9418352721fb600a
hibit-at <hibit_at@hotmail.com>
2020-08-24 19:39:17 +0900

    abs time

--- app/views.py
+++ app/views.py
@@ -46,7 +46,7 @@ def json_create(r,w,h,a):
         next_eul = 360*(i+1)/need_step
 
         #位置のセット
-        j["Movements"][i]["Duration"] = 0.1
+        j["Movements"][i]["Duration"] = 1/40
         j["Movements"][i]["StartPos"]["x"] = r*math.cos(rad)
         j["Movements"][i]["StartPos"]["y"] = h
         j["Movements"][i]["StartPos"]["z"] = r*math.sin(rad)
